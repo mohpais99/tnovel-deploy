99e08478f6cb499989f8ce7c9a49c3ec6403b1f9
mohpais <mohamad.pais@ipm>
2021-08-11 21:34:51 +0700

    genre controller

--- controllers/genre-controller.js
+++ controllers/genre-controller.js
@@ -5,13 +5,30 @@ class GenreController {
         try {
             await query('SELECT * FROM genres ORDER BY id')
                 .then(res => {
-                    console.log(res.rows);
+                    res.status(200).json({'data': res.rows});
                 })
                 .catch(err => {
                     console.log(err);
+                    res.status(500).json({'message': err});
                 })
         } catch (error) {
-            res.status(500).json({'message': 'Something wrong!'});
+            res.status(500).json({'message': error});
+        }
+    }
+    async createGenre(req, res) {
+        const {name} = req.body;
+        try {
+            await query(`INSERT INTO genres(name) VALUES ($1);`, [name])
+                .then(res => {
+                    // const message = `Berhasil membuat novel dengan id = ${id}`
+                    res.status(200).json({'data': res.rowCount, 'message': res});
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json({'message': err});
+                })
+        } catch (error) {
+            res.status(500).json({'message': error});
         }
     }
     async getGenreById(req, res) {
@@ -20,6 +37,7 @@ class GenreController {
             await query('SELECT * FROM genres WHERE id=$1', [id])
                 .then(res => {
                     console.log(res.rows);
+                    res.status(200).json({'data': res.rows, 'message': res});
                 })
                 .catch(err => {
                     console.log(err);
